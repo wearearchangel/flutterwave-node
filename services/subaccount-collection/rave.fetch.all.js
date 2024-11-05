eef575fd85f806176affc6b3624074a7e389421e
Michael Mwaura <michaelwmwaura@gmail.com>
2024-11-05 14:33:48 +0300

    feat: Allow collection subaccounts to be filtered by subaccount_id

--- services/subaccount-collection/rave.fetch.all.js
+++ services/subaccount-collection/rave.fetch.all.js
@@ -6,6 +6,7 @@ const { listSchema } = require("../schema/base");
 const newSchema = listSchema.concat(
   joi.object({
     start_date: joi.string().isoDate().default("2016-01-01T00:00:00.000Z"),
+    subaccount_id: joi.string().trim().max(100),
   })
 );
 
@@ -16,6 +17,17 @@ async function service(data = {}, _rave) {
 
   const { body: response } = await _rave.request(`/v3/subaccounts?`, data);
   logger(`Fetch all collection subaccounts`, _rave);
+
+  if (data?.subaccount_id) {
+    response.data = response.data.filter((r) => {
+      return r.subaccount_id === data.subaccount_id;
+    });
+
+    response.meta.page_info.total = response.data.length;
+    response.meta.page_info.current_page = 1;
+    response.meta.page_info.total_pages = 1;
+  }
+
   return response;
 }
 
